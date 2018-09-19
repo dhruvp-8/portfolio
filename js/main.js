b3adb12811ce8343392e13cea53395aaf8bbb22c
Dhruv Patel <dhruvrp@dynamic.lan1>
2018-09-19 00:47:51 -0700

    Validated Email

--- js/main.js
+++ js/main.js
@@ -25,6 +25,12 @@ function autoType(elementClass, typingSpeed){
   },1500);
 }
 
+function validateEmail(email) 
+{
+  var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+  return re.test(email);
+}
+
 function SendEmail(){
   var email = document.getElementById('email').value.toString();
   var name = document.getElementById('name').value;
@@ -33,29 +39,38 @@ function SendEmail(){
     document.getElementById('errorMsg').hidden = false;
     document.getElementById('successMsg').hidden = true;
     document.getElementById('errorMail').hidden = true;
+    document.getElementById('errorFormat').hidden = true;
   }
   else{
-    msg = '<p><b>Name: </b></p>' + name + '<p><b>Email: </b></p>' + email + '<p><b>Message: </b></p>' + comments;
-    document.getElementById('errorMsg').hidden = true;
-    var service_id = 'gmail';
-    var template_id = 'feedback';
-    var user_id = 'user_sIYId73ReD5KieiK2TQxR'
-    var template_params = {
-      from_name: 'Portfolio',
-      reply_to: 'portfolio.dhruvrp@gmail.com',
-      to_email: 'dhruvpatel5738@gmail.com',
-      sender_email: email,
-      message_html: msg 
-    };
+    if(validateEmail(email)){
+      msg = '<p><b>Name: </b></p>' + name + '<p><b>Email: </b></p>' + email + '<p><b>Message: </b></p>' + comments;
+      document.getElementById('errorMsg').hidden = true;
+      var service_id = 'gmail';
+      var template_id = 'feedback';
+      var user_id = 'user_sIYId73ReD5KieiK2TQxR'
+      var template_params = {
+        from_name: 'Portfolio',
+        reply_to: 'portfolio.dhruvrp@gmail.com',
+        to_email: 'dhruvpatel5738@gmail.com',
+        sender_email: email,
+        message_html: msg 
+      };
 
-    emailjs.send(service_id,template_id,template_params, user_id).then(function(response) {
-       document.getElementById('successMsg').hidden = false; 
-       document.getElementById('errorMsg').hidden = true;
-       document.getElementById('errorMail').hidden = true;
-    }, function(error) {
-       document.getElementById('errorMail').hidden = false;
-    });;
-    
+      emailjs.send(service_id,template_id,template_params, user_id).then(function(response) {
+         document.getElementById('successMsg').hidden = false; 
+         document.getElementById('errorMsg').hidden = true;
+         document.getElementById('errorMail').hidden = true;
+         document.getElementById('errorFormat').hidden = true;
+      }, function(error) {
+         document.getElementById('errorMail').hidden = false;
+      });;
+    }
+    else{
+      document.getElementById('errorFormat').hidden = false;
+      document.getElementById('errorMsg').hidden = true;
+      document.getElementById('successMsg').hidden = true;
+      document.getElementById('errorMail').hidden = true;
+    } 
   }
 }
 
